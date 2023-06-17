080bdc1b56c3a699f5cd6d4e41be8a7d43e4a661
Admin <raju.7102000@gmail.com>
2023-06-17 20:37:03 +0530

    modified:   music_sharing_portal/settings.py 	modified:   music_sharing_portal/wsgi.py 	new file:   vercel.json

--- music_sharing_portal/settings.py
+++ music_sharing_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-12yjbcu@di)cjd-x1&*tqr_d=#y#wslt19mxpf(b57df4txl(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
