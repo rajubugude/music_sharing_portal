487c5d615e9631abf8aba9231a2e379a813a6878
Admin <raju.7102000@gmail.com>
2023-06-18 12:45:08 +0530

    modified:   static/assets/css/style.css 	modified:   static/assets/js/main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Regna
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
