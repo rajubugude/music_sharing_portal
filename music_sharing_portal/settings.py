25978802f155204352d2d6f26673f8e7fa10d434
Raju Bugude <88735632+rajubugude@users.noreply.github.com>
2023-06-19 08:04:54 +0530

    Update settings.py

--- music_sharing_portal/settings.py
+++ music_sharing_portal/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'raju.7102000@gmail.com'
-EMAIL_HOST_PASSWORD = 'gmnkhmsabuqjwtze'
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
 
 
 # Static files (CSS, JavaScript, Images)
